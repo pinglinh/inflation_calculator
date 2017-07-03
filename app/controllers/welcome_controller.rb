15e897853b620028a320f45d12c1cc73c14fe47e
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-07-03 20:36:27 +0100

    Index method now works in the controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    # if params["start_date"] == "2015-01-01" && params["end_date"] == "2016-01-01" && params["region"] == "Angus"
-    #   @new_inflation = 142142.7644*(102.7203033/100.7151099)
-    #   # print new_inflation
-    # else
-    #   p "do nothing"
-    # end
-      @start_index = UkHousePriceIndex.find_by(region: params["region"], date: params["start_date"])
-      @end_index = UkHousePriceIndex.find_by(region: params["region"], date: params["end_date"])
-
-      @start_price = params["start_price"].to_i
-
-      @new_inflation = if @end_index != nil
-        (@start_price * (@end_index.price_index/@start_index.price_index)).round
-      end
+    @start_price = params["start_price"].to_i
+    inflation = UkHousePriceIndex.inflation(
+      params["region"],
+      params["start_date"],
+      params["end_date"]
+    )
+    @new_price = if inflation != nil
+      (@start_price * inflation).round
+    end
   end
 end
