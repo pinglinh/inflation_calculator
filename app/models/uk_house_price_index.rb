76f781149d9a76b02a3ed3d7dbe9b8476a808c38
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-07-03 20:34:45 +0100

    Added inflation method to UkHousePriceIndex class which was extracted from the controller

--- app/models/uk_house_price_index.rb
+++ app/models/uk_house_price_index.rb
@@ -1,2 +1,15 @@
 class UkHousePriceIndex < ApplicationRecord
+  def self.inflation(region, start_date, end_date)
+
+    start_index = UkHousePriceIndex.find_by(region: region, date: start_date)
+    end_index = UkHousePriceIndex.find_by(region: region, date: end_date)
+
+    if start_index != nil && end_index != nil
+      end_index.price_index/start_index.price_index
+    else
+      nil
+    end
+
+  end
+
 end
